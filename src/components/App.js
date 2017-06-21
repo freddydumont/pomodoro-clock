fd3a887e9966242aec55638c6f436168d02616f6
freddydumont <frederickmorin18@gmail.com>
2017-06-21 10:24:42 -0400

    add constructor to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import '../style/App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // initial state
+    this.state = {
+      breakLength: 5,
+      sessionLength: 25,
+    };
+  }
+
   render() {
     return (
       <div className="App">
