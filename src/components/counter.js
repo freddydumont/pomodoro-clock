6c423a2f97785cbf793bc8ddfb0c4323e5910f8f
freddydumont <frederickmorin18@gmail.com>
2017-06-21 11:40:31 -0400

    Update Counter to be able to change state in App

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // counter component expects type (session or break) and length from app
-const Counter = (props) => {
-    // counter returns 
-    return (
-        console.log(props.type, props.length)
-    );
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    handleClick(e) {
+        const type = this.props.type;
+        const mod = e.target.innerHTML;
+        switch (mod) {
+            case "-":
+                this.props.handleLengthChange(type, mod);
+                break;
+            case "+":
+                this.props.handleLengthChange(type, mod);
+                break;
+            default: break;
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>{this.props.type}</h3>
+                <p onClick={this.handleClick}>-</p>
+                <p>{this.props.length}</p>
+                <p onClick={this.handleClick}>+</p>
+            </div>
+        );
+    }
 }
 
 export default Counter;
