648fd63741c3a5c0e6ab547c223ca34016b7fd65
freddydumont <frederickmorin18@gmail.com>
2017-06-23 13:55:43 -0400

    Implement handling of start and pause
    * move interval to a clock object in App with functions start and pause
    * handleStartPause() calls these functions

--- src/components/App.js
+++ src/components/App.js
@@ -15,53 +15,69 @@ class App extends Component {
       sessionLength: 25,
       label: "Session",
       countdownStarted: false,
-      countdown: { minutes: 0, seconds: 2 },
+      countdown: { minutes: 25, seconds: 0 },
     };
 
     this.handleLengthChange = this.handleLengthChange.bind(this);
     this.handleStartPause = this.handleStartPause.bind(this);
-  }
+
+    // creating a clock object to manage start and pause
+    // https://stackoverflow.com/questions/12487352/
+    this.clock = {
+      start: () => {
+        this.timer = setInterval(() => {
+          // substract 1 second from countdown
+          this.setState(prevState => {
+            return {
+              countdown: {
+                // using spread operator to destructure countdown to merge
+                ...prevState.countdown,
+                seconds: prevState.countdown.seconds - 1
+              }
+            };
+          });
+          // if countdown.seconds < 0, substract 1 minute and set second to 59
+          if (this.state.countdown.seconds < 0) {
+            this.setState(prevState => {
+              return {
+                countdown: {
+                  minutes: prevState.countdown.minutes - 1,
+                  seconds: 59
+                }
+              }
+            });
+          }
+          // if countdown.minutes < 0, stop interval and play the sound
+          if (this.state.countdown.minutes < 0) {
+            clearInterval(this.timer);
+            // using audio from external source
+            // local files output errors: DOMException: Failed to load because no
+            // supported source was found or Uncaught (in promise) DOMException: 
+            // Unable to decode audio data
+            const gong = new Audio("https://soundbible.com/grab.php?id=1496&type=mp3");
+            gong.play();
+          }
+        }, 1000)
+      },
+
+      pause: () => {
+        clearInterval(this.timer);
+      }
+    }
+  };
 
   handleStartPause() {
-    // start or pause the countdown according to prevState
+    // start or pause the clock according to countdownStarted
+    if (!this.state.countdownStarted) {
+      this.clock.start();
+    } else {
+      this.clock.pause();
+    }
+
+    // switch countdownStarted
     this.setState(prevState => {
       return { countdownStarted: !prevState.countdownStarted };
     });
-
-    // if initial start, start countdown according to countdown object
-    const timer = setInterval(() => {
-      // substract 1 second from countdown
-      this.setState(prevState => {
-        return {
-          countdown: {
-            // using spread operator to destructure countdown to merge
-            ...prevState.countdown,
-            seconds: prevState.countdown.seconds - 1
-          }
-        };
-      });
-      // if countdown.seconds < 0, substract 1 minute and set second to 59
-      if (this.state.countdown.seconds < 0) {
-        this.setState(prevState => {
-          return {
-            countdown: {
-              minutes: prevState.countdown.minutes - 1,
-              seconds: 59
-            }
-          }
-        });
-      }
-      // if countdown.minutes < 0, stop interval and play the sound
-      if (this.state.countdown.minutes < 0) {
-        clearInterval(timer);
-        // using audio from external source
-        // local files output errors: DOMException: Failed to load because no
-        // supported source was found or Uncaught (in promise) DOMException: 
-        // Unable to decode audio data
-        const gong = new Audio("https://soundbible.com/grab.php?id=1496&type=mp3");
-        gong.play();
-      }
-    }, 1000)
   }
 
   handleLengthChange(label, op) {
