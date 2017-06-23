2bbc6724496f8044862e3a209974d29257982b26
freddydumont <frederickmorin18@gmail.com>
2017-06-23 12:01:10 -0400

    Add audio at the end of timer

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ class App extends Component {
       sessionLength: 25,
       label: "Session",
       countdownStarted: false,
-      countdown: { minutes: 25, seconds: 0 },
+      countdown: { minutes: 0, seconds: 2 },
     };
 
     this.handleLengthChange = this.handleLengthChange.bind(this);
@@ -32,7 +32,6 @@ class App extends Component {
     const timer = setInterval(() => {
       // substract 1 second from countdown
       this.setState(prevState => {
-        console.log(prevState.countdown.seconds);
         return {
           countdown: {
             // using spread operator to destructure countdown to merge
@@ -55,7 +54,12 @@ class App extends Component {
       // if countdown.minutes < 0, stop interval and play the sound
       if (this.state.countdown.minutes < 0) {
         clearInterval(timer);
-        console.log("finished");
+        // using audio from external source
+        // local files output errors: DOMException: Failed to load because no
+        // supported source was found or Uncaught (in promise) DOMException: 
+        // Unable to decode audio data
+        const gong = new Audio("https://soundbible.com/grab.php?id=1496&type=mp3");
+        gong.play();
       }
     }, 1000)
   }
