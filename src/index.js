e8cc57e201ae7d137749bbce670312987a04d1f2
freddydumont <frederickmorin18@gmail.com>
2017-08-05 14:21:42 -0400

    Unregister service worker

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { unregister } from './registerServiceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'));
+unregister();
